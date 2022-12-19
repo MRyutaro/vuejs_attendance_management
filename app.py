d34dfecd40835a8e7b7107f4a29c3347658f6387
RyutaroMatsumoto <m2002r1028@icloud.com>
2022-12-19 22:19:59 +0900

    [add] オウム返しをするプログラムを作成

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/api", methods=['POST'])
+def hello():
+    result = {
+        "results": request.json["text"].split()
+    }
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
